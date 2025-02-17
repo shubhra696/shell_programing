83b092e5dfe5bec5022a9ddf869b2d17e2136360
shubhra696 <shubhraranjan07@gmail.com>
2025-02-17 10:02:41 +0530

    factorial.sh

---
+++ factorial.sh
@@ -0,0 +1,7 @@
+shell program to find factorial of a number.
+#! /bin/bash
+echo -n "Enter a number: read num
+fact=1 for (( 1=1; i <num; i# ))
+do fact=$((fact * i))
+done
+echo "Factorial of $num is $fact"
