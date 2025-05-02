db40b8870b6a0f9ad9c24d1cdde85342acf38013
shubhra696 <shubhraranjan07@gmail.com>
2025-05-03 00:34:14 +0530

    Create SJF.sh

---
+++ SJF.sh
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+struct Process {
+    int pid, at, bt, ct, tat, wt, done;
+};
+
+int main() {
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    struct Process p[n];
+
+    for (int i = 0; i < n; i++) {
+        p[i].pid = i + 1;
+        printf("Enter Arrival Time and Burst Time for P%d: ", i + 1);
+        scanf("%d %d", &p[i].at, &p[i].bt);
+        p[i].done = 0;
+    }
+
+    int time = 0, done = 0;
+
+    while (done < n) {
+        int idx = -1, min_bt = 9999;
+
+        for (int i = 0; i < n; i++) {
+            if (!p[i].done && p[i].at <= time && p[i].bt < min_bt) {
+                min_bt = p[i].bt;
+                idx = i;
+            }
+        }
+
+        if (idx != -1) {
+            time += p[idx].bt;
+            p[idx].ct = time;
+            p[idx].tat = p[idx].ct - p[idx].at;
+            p[idx].wt = p[idx].tat - p[idx].bt;
+            p[idx].done = 1;
+            done++;
+        } else {
+            time++;
+        }
+    }
+
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+    float totalTAT = 0, totalWT = 0;
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].pid, p[i].at, p[i].bt, p[i].ct, p[i].tat, p[i].wt);
+        totalTAT += p[i].tat;
+        totalWT += p[i].wt;
+    }
+
+    printf("\nAverage Turnaround Time = %.2f\n", totalTAT / n);
+    printf("Average Waiting Time = %.2f\n", totalWT / n);
+
+    return 0;
+}
